fe0aa5df2ef5c1bb650b7740ef4632bdb2e072a8
Tobin Waters <tkwaterss@gmail.com>
2023-01-26 08:40:47 -0700

    Updated with alternate solutions from other students

--- challenge-find-the-range/findTheRange.js
+++ challenge-find-the-range/findTheRange.js
@@ -24,3 +24,17 @@ for (let i = 1; i < array.length; i++) {
     arrMin = Math.min(arrMin, array[i]);
 }
 console.log(arrMax, arrMin);
+
+//Solution #3 - Brad Tolson
+// let lowest = Math.min(...array);
+// console.log('Lowest Number:' + lowest);
+// let highest = Math.max(...array);
+// console.log('Highest Number:' + highest);
+
+//Solution #4 - BatuhanSundseth
+// let min, max = array[0]
+// for (i = 0; i <= array.length; i++) {
+//   array[i] < min ? min = array[i] : (array[i] > max ? max = array [i] : min = min)
+// }
+// console.log("The smallest number is: " + min)
+// console.log("The largest number is: " + max)
